71debacfcd9b25c0cfe3fce0cc7586a2ec6917d1
Alejandro Reyes <alejandroereyes@gmail.com>
2025-03-03 17:00:48 -0600

    wrap url endpoints behind auth - restrict Urls#index to current user

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,17 @@
 class UrlsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @urls = Url.all
+    @urls = Url.where(user_id: current_user.id)
+
+    respond_to do |format|
+      format.html {  }
+      format.turbo_stream
+    end
   end
 
   def show
-    @url = Url.find(params[:id])
+    @url = Url.where(id: current_user.id).find(params[:id])
   end
 
   def new
@@ -13,9 +20,10 @@ class UrlsController < ApplicationController
 
   def create
     @url = Url.new(create_params)
+    @url.user = current_user
     Urls::Create.call(@url)
 
-    redirect_to root_path
+    redirect_to new_url_path
   end
 
   private
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,8 @@
 class Url < ApplicationRecord
+  belongs_to :user
+
+  validates :original_url, presence: true
+  validates :short_code, presence: true
+
   broadcasts_to ->(url) { :urls }
 end
