bb6616f12468b8c6ac22dd44d8179c1e61f2759d
Alejandro Reyes <alejandroereyes@gmail.com>
2025-03-03 10:10:22 -0600

    rename shortener service

--- app/services/urls/shortener.rb
+++ app/services/urls/shortener.rb
@@ -1,11 +1,11 @@
 # https://zmerrychristmas.github.io/posts/shortened-url-by-ruby-on-rails/
 
 module Urls
-  class Encode
-    ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(//)
+  class Shortener
+    ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.".split(//)
     LENGTH = 7
 
-    def self.bijective_encode(i)
+    def self.call(i)
       # from http://refactormycode.com/codes/125-base-62-encoding
       # with only minor modification
       return ALPHABET[0] if i == 0
@@ -16,15 +16,6 @@ module Urls
         i /= base
       end
       s.reverse[0...LENGTH]
-    end
-
-    def self.bijective_decode(s)
-      # based on base2dec() in Tcl translation
-      # at http://rosettacode.org/wiki/Non-decimal_radices/Convert#Ruby
-      i = 0
-      base = ALPHABET.length
-      s.each_char { |c| i = i * base + ALPHABET.index(c) }
-      i
     end
   end
 end
